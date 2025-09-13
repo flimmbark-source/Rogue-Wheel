67303517e898415b28fe86331508d25bb47c6c3a
flimmbark-source <flimmbark@gmail.com>
2025-09-13 12:04:17 -0700

    Fix vite.config.ts to dynamically import React plugin for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(async () => {
+  const react = await import('@vitejs/plugin-react');
+  return {
+    plugins: [react.default()],
+  };
+});
